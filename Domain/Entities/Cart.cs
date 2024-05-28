49fb585af20eea3ff9237d80958c22096020f35d
ApxNord <nord-life17@yandex.ru>
2024-05-28 20:58:09 +0300

    Добавление корзины

---
+++ Domain/Entities/Cart.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Cart
+    {
+        private List<CartLine> cartLineList
+            = new List<CartLine>();
+
+        public void AddItem(Game game, int quantity)
+        {
+            CartLine line = cartLineList
+                .Where(g => g.Game.Id == game.Id)
+                .FirstOrDefault();
+
+            if (line == null)
+                cartLineList .Add(
+                    new CartLine
+                    {
+                        Game = game,
+                        Quantity = quantity
+                    });
+            else 
+                line.Quantity += quantity;
+        }
+        public void RemoveItem(Game game)
+        { 
+            cartLineList.RemoveAll(g => 
+            g.Game.Id == game.Id);
+        }
+        public void Clear()
+        {
+            cartLineList.Clear();
+        }
+        public IEnumerable<CartLine> Lines
+        {
+            get { return cartLineList; }
+        }
+        public decimal ComputeTotalPrice()
+        {
+            return cartLineList
+                .Sum(g => g.Game.Price * g.Quantity);
+        }
+    }
+    public class CartLine
+    {
+        public Game Game { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ WebUI/Controllers/CartController.cs
@@ -0,0 +1,70 @@
+﻿using Domain.Abstract;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebUI.Models;
+
+namespace WebUI.Controllers
+{
+    public class CartController : Controller
+    {
+        private IGameRepository gameRepository;
+        public CartController (IGameRepository gameRepository)
+        {
+            this.gameRepository = gameRepository;
+        }
+
+        public ViewResult Index(string returnUrl) {
+            return View(new CartIndexViewModel
+            {
+                Cart = GetCart(),
+                ReturnUrl = returnUrl
+            });
+        }
+
+        public RedirectToRouteResult AddToCart(
+            int Id, string returnUrl)
+        {
+            Game game = gameRepository.Games
+                .FirstOrDefault(x => x.Id == Id);
+
+            if (game != null) {
+                GetCart().AddItem(game, 1);
+            }
+
+            return RedirectToAction("Index", new {returnUrl});
+        }
+        public RedirectToRouteResult RemoveFromCart(int Id,
+            string returnUrl)
+        {
+            Game game = gameRepository.Games
+               .FirstOrDefault(x => x.Id == Id);
+
+            if (game != null)
+            {
+                GetCart().RemoveItem(game);
+            }
+
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        public Cart GetCart()
+        {
+            Cart cart = (Cart)Session["Cart"];
+            if (cart == null)
+            {
+                cart = new Cart();
+                Session["Cart"] = cart;
+            }
+            return cart;
+        }
+
+        public PartialViewResult Summary (Cart cart)
+        {
+            return PartialView(GetCart());
+        }
+    }
+}
